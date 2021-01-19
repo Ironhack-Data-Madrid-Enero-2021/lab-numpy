ff64163d837c664242278054d46653a7a3016e1e
xarlomi <cportillogarrigues@gmail.com>
2021-01-20 00:26:38 +0100

    last

--- your-code/main.py
+++ your-code/main.py
@@ -9,15 +9,21 @@ print(np)
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.randint(0, 50,(2, 3, 5)) 
+"print(a)"
 
-
+a = np.random.rand(2, 3, 5)
+"print(a)"
+a= np.random.random((2, 3, 5))
+"print(a)"
 #4. Print a.
 
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.full((5, 2, 3), 1) 
+"print(b)"
 
 
 #6. Print b.
@@ -25,43 +31,62 @@ print(np)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+"""
+No. Either by using a as the filling value of b in the full function (above), which gives a value error 
+since input of array a isnt transposable due to different shapes. Or by setting them equal which gives a boolean
+result false
+"""
+"a == b"
 
 
 
 #8. Are you able to add a and b? Why or why not?
 
+"no because they dont have the same shape"
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = b.reshape(2, 3, 5)
 
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+d = c + a
+
+
 
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+"print(a)"
+"print(d)"
 
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e= c * a
+
 
 
 #13. Does e equal to a? Why or why not?
-
+"yes because e is == 1"
+e == a
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+
+d_min= d.min()
+d_max= d.max()
+d_mean= d.mean()
 
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty([2, 3, 5])
 
 
 
@@ -74,6 +99,23 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+for functions in f:
+        for matrix in d:
+                if d_min:
+                        functions = 0
+                if d_min < matrix and matrix < d_mean:
+                        functions = 25 
+                if d_mean < matrix and matrix < d_max:
+                        functions= 75
+                if d_mean:
+                        functions = 50
+                if d_max:
+                        functions = 100
+                print(f.any())
+                
+               
+      [0, 25, ]           
+
 
 
 
