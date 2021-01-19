44458ee9f06c6cb7a0896f0ecb99ded71f558a1b
Florencio Gutiérrez <floren.1992@gmail.com>
2021-01-19 17:00:03 +0100

    numpypyyyy

--- your-code/main.py
+++ your-code/main.py
@@ -13,62 +13,98 @@ print(np.show_config())
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
 
+a = np.random.random((2,3,5))
 
+array = np.arange(30).reshape(2,3,5)
 
 
 
 #4. Print a.
 
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3))
 
 
 #6. Print b.
 
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+a.size
+b.size
 
+a.size == b.size
+
+True
 
 
 #8. Are you able to add a and b? Why or why not?
 
+other = np.add(a,b)
+#operands could not be broadcast together with shapes (2,3,5) (5,2,3) 
+#tienen misma dimensión pero diferente "distribución"
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = b.transpose().reshape(2,3,5)
 
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+
+d = np.add(a,c)
+
+print(d)
 
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
 
-
+#.shape y .ndim son iguales, cambian los valores ya que el resultado de d es el de a + 1.
 
 #12. Multiply a and c. Assign the result to e.
 
+e = np.multiply(a, c)
+
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
 
 
+e == a
 
+array([[[ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True]],
+
+       [[ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True],
+        [ True,  True,  True,  True,  True]]])
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.nanmax(d) 
 
 
+d_min = np.nanmin(d)
+
+d_mean = np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty([2,3,5])
 
 
 """
