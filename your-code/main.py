94461bab711bb473c6701895d30e8956206466ac
CristinaCallejo <cristinapazcallejo@gmail.com>
2021-01-19 22:14:52 +0100

    done

--- your-code/main.py
+++ your-code/main.py
@@ -1,74 +1,74 @@
 #1. Import the NUMPY package under the name np.
+
 import numpy as np
 
 #2. Print the NUMPY version and the configuration.
+
 print(np.version.version)
 '''
 1.19.5
 '''
 
-
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+
 a = np.random.rand(2,3,5)
+
 #4. Print a.
 
 print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
+
 b = np.ones((5,2,3))
 
-
 #6. Print b.
+
 print(b)
 
-
 #7. Do a and b have the same size? How do you prove that in Python code?
+
 print(a.shape == b.shape)
 
-
 #8. Are you able to add a and b? Why or why not?
-a + b
-''' a + b are not equal in shape'''
 
+'''No, a + b are not equal in shape'''
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+
 c = b.reshape(2,3,5)
 
-
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+
 a + c
 d = a + c
 
-
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+
 print(a)
 print(d)
 '''it has added 1 to each value in d'''
 
 #12. Multiply a and c. Assign the result to e.
+
 e = a * c
 print(e)
-#13. Does e equal to a? Why or why not?
-e == a
-'''Comparing arrays with Boolean == compares the elements of array'''
-'''In this case, e == a in ALL the elements.''' 
 
+#13. Does e equal to a? Why or why not?
+
+e == a
+'''Comparing arrays with Boolean == compares the elements of array, in this case, e == a in ALL the elements''' 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+
 d_max = d.max()
 d_min = d.min()
 d_mean = d.mean()
-print ("d_max = ",d.max())
-print ("d_min = ",d.min())
-print ("d_mean = ",d.mean())
-
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-d.shape
+
 f = np.empty((2,3,5))
-print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -79,18 +79,11 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
 f[(d_min < d) & (d < d_min)] = 25
-print (f)
 f[(d_mean < d) & (d < d_max)] = 75
 f[d_mean == d] = 50
 f[d_min == d] = 0
 f[d_max == d] = 100 
-
-
-
-        
-
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -127,3 +120,5 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+fjeje = dict(A)
