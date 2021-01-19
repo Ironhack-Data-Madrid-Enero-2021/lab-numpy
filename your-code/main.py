a4a26f0ad320aa28706c4c8aa411531af4175530
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-01-19 17:40:45 +0100

    Terminado hasta el punto 18

--- your-code/main.py
+++ your-code/main.py
@@ -4,65 +4,88 @@ import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.randint(0, 100, (2, 3, 5))
 
 #4. Print a.
 
-
+print("a:")
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5, 2, 3))
 
 #6. Print b.
 
-
+print("b:")
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print("Do they have the same shape?: ", a.shape == b.shape)
+print("Do they have the same size?: ", a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+        #print(a+b)
+print("We can't add a and b because their shape is not the same, they don't have the same dimensions")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = b.reshape((2, 3, 5))
+print("c:")
+print(c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
+print("d:")
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print("a:")
+print(a)
+print("d:")
+print(d)
+print("With reshape it works because you can add two matrixes with the same dimensions")
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e=a*c
+print("e: ")
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
 
-
+print("Is e=a? ")
+print(e==a)
+print("It is the same because you multiply by 1, c is a matrix with all the elements equal to 1.")
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
+print(f"The maximum value is {d_max}, the minimum is {d_min}, and the mean is {d_mean.round(2)}.")
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty((2, 3, 5))
+print("f: ")
+print(f)
 
 
 """
@@ -75,6 +98,11 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+f[(d_mean>d) & (d>d_min)]=25
+f[(d_max>d) & (d>d_mean)]=75
+f[d==d_mean]=50
+f[d==d_min]=0
+f[d==d_max]=100
 
 
 
@@ -99,6 +127,11 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print("e: ")
+print(e)
+print("New f: ")
+print(f)
+print("I have my expected f :)")
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +145,12 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+
+f[(d_mean>d) & (d>d_min)]="A"
+f[(d_max>d) & (d>d_mean)]="B"
+f[d==d_mean]="C"
+f[d==d_min]="D"
+f[d==d_max]="E"
+print("New f: ")
+print(f)
