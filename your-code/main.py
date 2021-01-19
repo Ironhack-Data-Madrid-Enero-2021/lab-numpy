ecab3fc6f74040adc42a2f766cbb258570952060
Ángela <angela.navarro.gonzalez@gmail.com>
2021-01-19 17:39:09 +0100

    numpy Angela

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,107 @@
 #1. Import the NUMPY package under the name np.
 
-
-print ("hola)
+import numpy as np
        
 #2. Print the NUMPY version and the configuration.
 
+print (np.__version__)
+print (np.show_config())
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.randint(0, 100, size=(2, 3, 5))
+a_check = a.shape
 
 
 #4. Print a.
-
-
+print (a)
+print (a_check)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5, 2, 3))
+b_check = b.shape
 
 #6. Print b.
 
-
+print (b)
+print (b_check)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+a_check = a.shape
+b_check = b.shape
 
-
+print (a_check, b_check)
 
 #8. Are you able to add a and b? Why or why not?
 
+'''additon = np.add (a,b)
 
+"print (addition)"
+
+print ("No, because the shape of each array is different.")'''
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = np.transpose(b).reshape(2,3,5)
+c_check = c.shape
+
+print (c)
+
+print (c_check)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = np.add (a,c)
 
+print (d)
+
+print ("Because the shape of each array is the same")
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print (a)
 
+print (d)
+
+print ("The relation is that it seems that an 'np.ones' array has been added to each, an each value has been increased adding 1.")
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e = np.multiply (a,c)
 
+print (e)
 
 #13. Does e equal to a? Why or why not?
 
-
+print ("Yes, because 'c' is the traspose of 'b' where each value is 1, so when you multipy a and c, the result of a is the same.")
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max ()
+d_min = d.min ()
+d_mean = d.mean ()
 
+print (d_max)
 
+print (d_min)
+
+print (d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty ((2,3,5))
 
+''' f = np.empty(shape = d.shape, dtype=np.str)'''
+
+print (f)
 
 
 """
@@ -75,6 +113,26 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+
+def mapping_function(f):
+
+        if value > d_min and value < d_mean:
+            return '25'
+        elif value > d_mean and value < d_max:
+            return '75'
+        elif value > d_max:
+            return '50'  
+        elif value == d_mean:
+            return '50' 
+        elif value == d_min:
+            return '50'
+        else:
+            return '100'
+
+mapped_values = map(mapping_function, d)
+
+print (mapped_values)
+
 
 
 
